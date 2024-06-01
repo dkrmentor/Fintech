dab20c7f22a2173dc12bbbf33a4796e4f6c6e08e
dkrmentor <dkrmentor@gmail.com>
2024-06-02 03:57:59 +0500

    layout

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Table, Input } from "antd";
+import { Table, Input, Layout } from "antd";
+
 import axios from "axios";
 
 const App = () => {
   const [dataSource, setDataSource] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const { Header, Content, Footer } = Layout;
 
   const fetchNabData = async () => {
     try {
@@ -59,7 +61,8 @@ const App = () => {
       dataIndex: "productCategory",
       key: "productCategory",
       filteredValue: searchText ? [searchText] : null,
-      onFilter: (value, record) => record.productCategory.toLowerCase().includes(value.toLowerCase())
+      onFilter: (value, record) =>
+        record.productCategory.toLowerCase().includes(value.toLowerCase()),
     },
     {
       title: "Name",
@@ -79,14 +82,46 @@ const App = () => {
   ];
 
   return (
-    <>
-      <Input.Search
-        placeholder="Search"
-        style={{ marginBottom: 8 }}
-        onSearch={setSearchText}
-      />
-      <Table dataSource={dataSource} columns={columns} />
-    </>
+    <Layout className="layout">
+      <Header
+        style={{
+          background: "#ffffff",
+          boxShadow: " 0 1px 2px 0 rgba(0, 0, 0, 0.03), 0 1px 6px -1px rgba(0, 0, 0, 0.02), 0 2px 4px 0 rgba(0, 0, 0, 0.02)",        
+          
+
+        }}
+      >
+       <div className="logo" style={{display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",}}>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNV1Tr-PvO8PH5uh-fjsry0KgGBwUXSgORaw&s" // Replace with your logo path
+            alt="Logo"
+            style={{ height: "60px" }} // Adjust the height as needed
+          />
+        </div>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{
+          padding: "50px 200px",
+        }}
+      >
+        <Input.Search
+          placeholder="Search"
+          style={{ marginBottom: 8 }}
+          onSearch={setSearchText}
+        />
+        <Table dataSource={dataSource} columns={columns} />
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Design ©2024
+      </Footer>
+    </Layout>
   );
 };
 
